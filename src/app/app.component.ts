046305bcf93e2b787940112a298969ea3d5facca
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2025-02-09 08:53:31 -0600

    inicio de notificaciones push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { CommonModule } from "@angular/common";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ApiInterceptor } from "./shared/services/api-interceptor.service";
 import { SessionService } from "./shared/services/session.service";
-import { ERol } from "./shared/constants/rol.enum"; 
+import { ERol } from "./shared/constants/rol.enum";
 // Importa PrimeNG Dialog y Button
 import { DialogModule } from "primeng/dialog";
 import { ButtonModule } from "primeng/button";
-
+import { SwPush } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 @Component({
   selector: "app-root",
   standalone: true,
@@ -18,33 +19,46 @@ import { ButtonModule } from "primeng/button";
     NgxUiLoaderModule,
     CommonModule,
     DialogModule, // ✅ Importación del Dialog de PrimeNG
-    ButtonModule  // ✅ Importación de los Botones de PrimeNG
+    ButtonModule, // ✅ Importación de los Botones de PrimeNG
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ApiInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
   ],
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
   showWelcomeMessage = false;
   userROL: string = "";
 
+  publicKey: string = environment.publicKey; // Este es el valor que debes obtener en la consola de Firebase.
+
   constructor(
+    private swPush: SwPush,
     private ngxService: NgxUiLoaderService,
     private sessionService: SessionService
-  ) {}
+  ) {
+    this.notificar();
+  }
+  notificar() {
+    this.swPush
+      .requestSubscription({ serverPublicKey: this.publicKey })
+      .then((sub) => {
+        const token = JSON.parse(JSON.stringify(sub));
+        console.log("JSON+++++++++");
+      });
+  }
 
   ngOnInit() {
-    this.showWelcomeMessage = this.isUserLoggedIn()
+    this.showWelcomeMessage = this.isUserLoggedIn();
     if (typeof sessionStorage !== "undefined") {
       if (!sessionStorage.getItem("firstSession") && !this.isUserLoggedIn()) {
         this.ngxService.start();
-        
+
         setTimeout(() => {
           this.ngxService.stop();
           sessionStorage.setItem("firstSession", "true");
@@ -74,4 +88,6 @@ export class AppComponent implements OnInit {
   cerrarMensaje() {
     this.showWelcomeMessage = false;
   }
+
+  // notificacion push
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,7 @@ export const environment = {
   api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
   // api: 'http://localhost:4000/api/v1', //!Local
   serviceWorker: true,
+  publicKey:"BA8CjWNIlvuEogKv1eRaHP0Ro6uo3wfq4TWfTJk1-OIqjuHcxKeVrX25ItmnmwhfskcvW9kqX39kO1jpzhnAc5Y"
+
   // keyCapchat: '6LfdbGcqAAAAAEtcCSBTxBMZmBIFO5gIIH_iDzMc', //? Local
 };
