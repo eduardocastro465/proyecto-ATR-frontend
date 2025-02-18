48ffbb05a2e1678cd2273c4a657e9e314c45caca
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2025-02-18 07:31:13 -0600

    publicKey:     BPqUE0OuQtFwPMDzFFttBK-aM3oJePkk_vsQ0OPmRQVJwWYQY1gq1U7mxFPRuSUR85rwBiU1ynfCsExlCIt40fk,

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 export const environment = {
   production: false,
   // api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
-  api: 'http://localhost:4000/api/v1', //!Local
+  api: "http://localhost:4000/api/v1", //!Local
   serviceWorker: true,
-  publicKey:"BA8CjWNIlvuEogKv1eRaHP0Ro6uo3wfq4TWfTJk1-OIqjuHcxKeVrX25ItmnmwhfskcvW9kqX39kO1jpzhnAc5Y"
+  // publicKey:"BPqUE0OuQtFwPMDzFFttBK-aM3oJePkk_vsQ0OPmRQVJwWYQY1gq1U7mxFPRuSUR85rwBiU1ynfCsExlCIt40fk"
+  publicKey:
+    "BPqUE0OuQtFwPMDzFFttBK-aM3oJePkk_vsQ0OPmRQVJwWYQY1gq1U7mxFPRuSUR85rwBiU1ynfCsExlCIt40fk",
 
   // keyCapchat: '6LfdbGcqAAAAAEtcCSBTxBMZmBIFO5gIIH_iDzMc', //? Local
 };
