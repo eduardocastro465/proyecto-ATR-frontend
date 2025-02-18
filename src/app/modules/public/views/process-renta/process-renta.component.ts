2d5f3cc47e368fbc519a1a60668469da4a04ab63
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2025-02-18 07:38:08 -0600

    cmabio de llaves

--- src/app/modules/public/views/process-renta/process-renta.component.ts
+++ src/app/modules/public/views/process-renta/process-renta.component.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ProductoService } from '../../../../shared/services/producto.service';
-import { SwPush } from '@angular/service-worker';
-import { environment } from '../../../../../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ProductoService } from "../../../../shared/services/producto.service";
+import { SwPush } from "@angular/service-worker";
+import { environment } from "../../../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { NgxUiLoaderService } from "ngx-ui-loader";
 
 @Component({
-  selector: 'app-process-renta',
-  templateUrl: './process-renta.component.html',
-  styleUrls: ['./process-renta.component.scss']
+  selector: "app-process-renta",
+  templateUrl: "./process-renta.component.html",
+  styleUrls: ["./process-renta.component.scss"],
 })
 export class ProcessRentaComponent implements OnInit {
-
   isLoading: boolean = false;
-  productId: string = '';
+  productId: string = "";
   Detalles: any;
 
   // Variables del formulario de renta
-  arrendador: string = '';
-  arrendatario: string = '';
-  fechaInicio: string = '';
-  fechaFin: string = '';
-  direccionInmueble: string = '';
+  arrendador: string = "";
+  arrendatario: string = "";
+  fechaInicio: string = "";
+  fechaFin: string = "";
+  direccionInmueble: string = "";
   montoRenta: number | null = null;
   contratoGenerado: boolean = false;
   publicKey: string = environment.publicKey;
@@ -41,7 +40,7 @@ export class ProcessRentaComponent implements OnInit {
     this.scrollToTop();
     this.generarToken(); // Generamos el token antes de enviar los datos
 
-    this.productId = this.activatedRoute.snapshot.params['id'];
+    this.productId = this.activatedRoute.snapshot.params["id"];
 
     this.productoS_.obtenerDetalleProductoById(this.productId).subscribe({
       next: (response) => {
@@ -51,13 +50,13 @@ export class ProcessRentaComponent implements OnInit {
       },
       error: (err) => {
         this.isLoading = false;
-        console.error('Error al obtener detalles:', err);
-      }
+        console.error("Error al obtener detalles:", err);
+      },
     });
   }
 
   scrollToTop(): void {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
   // Método de submit del formulario
@@ -68,13 +67,13 @@ export class ProcessRentaComponent implements OnInit {
 
   // Método para generar el token y enviarlo al backend
   generarToken(): void {
-    if ('serviceWorker' in navigator && this.swPush) {
-      this.swPush.requestSubscription({ serverPublicKey: this.publicKey })
+    if ("serviceWorker" in navigator && this.swPush) {
+      this.swPush
+        .requestSubscription({ serverPublicKey: this.publicKey })
         .then((sub) => {
           const token = JSON.stringify(sub);
-         console.log("enviar=>",token)
-         
-         
+          console.log("enviar=>", token);
+
           const data = {
             arrendador: this.arrendador,
             arrendatario: this.arrendatario,
@@ -83,30 +82,34 @@ export class ProcessRentaComponent implements OnInit {
             direccionInmueble: this.direccionInmueble,
             montoRenta: this.montoRenta,
             productId: this.productId,
-            token: token // Token generado
+            token: token, // Token generado
           };
-          
+
           this.enviarTokenAlBackend(token); // Enviar el token al backend
         })
         .catch((err) => {
-          console.error('Error al suscribirse a notificaciones:', err);
-          alert('Hubo un problema al suscribirse a las notificaciones. Por favor, asegúrese de que el navegador soporte Service Workers.');
+          console.error("Error al suscribirse a notificaciones:", err);
+          alert(
+            "Hubo un problema al suscribirse a las notificaciones. Por favor, asegúrese de que el navegador soporte Service Workers."
+          );
           this.isLoading = false; // Desactiva el loader en caso de error
         });
     } else {
-      console.error('Service Workers no están habilitados en este navegador.');
-      alert('El navegador no soporta notificaciones push.');
+      console.error("Service Workers no están habilitados en este navegador.");
+      alert("El navegador no soporta notificaciones push.");
       this.isLoading = false; // Desactiva el loader si no es compatible
     }
   }
-  
 
   // Método para enviar el token de notificación al backend
   enviarTokenAlBackend(token: string): void {
-    this.http.post('http://localhost:4000/api/v1/enviar-notificacion/ejemplo', { token })
+    this.http
+      .post("http://localhost:4000/api/v1/enviar-notificacion/ejemplo", {
+        token,
+      })
       .subscribe(
-        () => console.log('Token enviado al backend correctamente'),
-        (err) => console.error('Error al enviar el token al backend:', err)
+        () => console.log("Token enviado al backend correctamente"),
+        (err) => console.error("Error al enviar el token al backend:", err)
       );
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   // api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
-  api: "http://localhost:4000/api/v1", //!Local
+  api: 'http://localhost:4000/api/v1', //!Local
   serviceWorker: true,
-  // publicKey:"BPqUE0OuQtFwPMDzFFttBK-aM3oJePkk_vsQ0OPmRQVJwWYQY1gq1U7mxFPRuSUR85rwBiU1ynfCsExlCIt40fk"
-  publicKey:
-    "BPqUE0OuQtFwPMDzFFttBK-aM3oJePkk_vsQ0OPmRQVJwWYQY1gq1U7mxFPRuSUR85rwBiU1ynfCsExlCIt40fk",
+
+  publicKey:"BLYrs4NfSkkjBeVIMgH2ANO4Hcd3FrGKYXabe5L6jpURiePXV9cbl4IewwLjzdszahWAb8SaiPNmyMz6mywr6KY",
+  privateKey:"keH3GwgD0noSJ7IHg_DLwMRP5WM8T0xlRRb2OG-bjFI"
 
   // keyCapchat: '6LfdbGcqAAAAAEtcCSBTxBMZmBIFO5gIIH_iDzMc', //? Local
 };
